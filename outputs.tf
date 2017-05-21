602fa87d069256e4e9ccea5050758a970a550d92
root <root@kolla22.labs.local>
2017-05-21 12:53:45 +0800

    modify user data script.

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
-output "address" {
+#output "address" {
 #  value = "${openstack_networking_floatingip_v2.float6.address}"
-  value = "${openstack_compute_instance_v2.master1.network/fixed_ip_v4}"
-}
+#  value = "${openstack_compute_instance_v2.master1.network.fixed_ip_v4}"
+#}
 
 
--- yum_proxy.sh
+++ yum_proxy.sh
@@ -2,9 +2,16 @@
 
 sed -i '$a proxy=http://opscloud:cloud0518@10.53.13.245:3128/' /etc/yum.conf
 yum -y install wget git net-tools bind-utils iptables-services bridge-utils bash-completion
+git config --global http.proxy  http://opscloud:cloud0518@10.53.13.245:3128
+git config --global http.proxy  https://opscloud:cloud0518@10.53.13.245:3128
 apt-get update
 yum -y install NetworkManager
 systemctl start NetworkManager.service
 systemctl status NetworkManager.service
 systemctl enable NetworkManager.service
+systemctl start iptables
+systemctl enable iptables
+yum install -y docker-1.12.6
+systemctl start docker.service
+systemctl enable docker.service
 systemctl reboot
